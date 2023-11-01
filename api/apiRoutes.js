0182dd03788e510689bb5d3422dd1dee121ce82b
jeyreichel <149537177+jeyreichel@users.noreply.github.com>
2023-11-01 02:17:25 -0700

    Update apiRoutes.js

--- api/apiRoutes.js
+++ api/apiRoutes.js
@@ -3,8 +3,6 @@ const axios = require('axios');
 const sgMail = require('@sendgrid/mail');
 const router = express.Router();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 router.get('/', async (req, res) => {
     try {
         res.json({
