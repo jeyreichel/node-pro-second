3c90a26e93ed2cc8c9d1e2a4a164aabfa43fb671
jeyreichel <149537177+jeyreichel@users.noreply.github.com>
2023-11-01 08:59:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ const cors = require('cors');
 const apiRouters = require('./api/apiRoutes');
 const sgMail = require('@sendgrid/mail');
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
 admin.initializeApp({
     credential: admin.credential.cert({
         // Add your Firebase Admin SDK credentials here
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
+        projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+        clientEmail: `${process.env.FIREBASE_CLIENT_EMAIL}`,
+        privateKey: `${process.env.FIREBASE_PRIVATE_KEY}`.replace(/\\n/g, '\n'),
+        databaseURL: `${process.env.FIREBASE_DATABASE_URL}`,
     }),
 });
 
 const app = express();
 const port = 5050;
-
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 app.use(cors());
 app.use(express.json({ extended: false }));
 app.use('/api', apiRouters);
